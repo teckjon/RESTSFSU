ad3de282a8775c93fcbd59b8f76e77928554f0d5
teck lim <tecklim@tecks-MacBook-Pro.local>
2016-06-04 14:29:13 -0700

    finished race district

--- RaceEthnicity.js
+++ RaceEthnicity.js
@@ -1,6 +1,35 @@
 var RaceEthnicity = {
     district1: [
-        {African-American: 4%, Asian-Pacific-Islander: 60%, Hispanic-Latino: 8%, Multiracial-Multiethnic: 9%, White: 20%, Other: 0%} 
+        {African-American: '4%', Asian-Pacific-Islander: '60%', Hispanic-Latino: '8%', Multiracial-Multiethnic: '9%', White: '20%', Other: '0%'} 
+    ],
+    district2: [
+        {African-American: '11%', Asian-Pacific-Islander: '24%', Hispanic-Latino: '11%', Multiracial-Multiethnic: '11%', White: '42%', Other: '1%'}
+    ],
+    district3: [
+        {African-American: '6%,' Asian-Pacific-Islander: '77%', Hispanic-Latino: '7%', Multiracial-Multiethnic: '3%', White: '6%', Other: '0%'}
+    ],
+    district4: [
+        {African-American: '3%', Asian-Pacific-Islander: '70%', Hispanic-Latino: '7%', Multiracial-Multiethnic: '3%', White: '6%', Other: '0%'}
+    ],
+    district5: [
+        {African-American: '39%', Asian-Pacific-Islander: '17%', Hispanic-Latino: '19%', Multiracial-Multiethnic: '9%', White: '16%', Other: '0%'}
+    ],
+    district6: [
+        {African-American: '15%', Asian-Pacific-Islander: '31%', Hispanic-Latino: '35%', Multiracial-Multiethnic: '6%', White: '12%', Other: '1%'}
+    ],
+    district7: [
+        {African-American: '8%', Asian-Pacific-Islander: '46%', Hispanic-Latino: '16%', Multiracial-Multiethnic: '9%', White: '22%', Other: '0%'}
+    ],
+    district8: [
+        {African-American: '13%', Asian-Pacific-Islander: '12%', Hispanic-Latino: '34%', Multiracial-Multiethnic: '11%', White: '30%', Other: '0%'}
+    ],
+    district9: [
+        {African-American: '6%', Asian-Pacific-Islander: '23%', Hispanic-Latino: '58%', Multiracial-Multiethnic: '5%', White: '3%', Other: '0%'}
+    ],
+    district10: [
+        {African-American: '34%', Asian-Pacific-Islander: '29%', Hispanic-Latino: '29%', Multiracial-Multiethnic: '5%', White: '3%', Other: '0%'}
+    ],
+    district11: [
+        {African-American: '7%', Asian-Pacific-Islander: '48%', Hispanic-Latino: '37%', Multiracial-Multiethnic: '4%', White: '4%', Other: '0%'}
     ]
-    district2: 
 }
