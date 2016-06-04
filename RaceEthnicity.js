3c3e1cfc69a56a101f825211b22060132c8bbf85
teck lim <tecklim@tecks-MacBook-Pro.local>
2016-06-04 14:11:28 -0700

    finished district 1

--- RaceEthnicity.js
+++ RaceEthnicity.js
@@ -1,5 +1,6 @@
 var RaceEthnicity = {
     district1: [
-        {African-American: 4%, Asian-Pacific-Islander: 60%, Hispanic-Latino: 8%, Multiracial-Multiethnic: 9%}
+        {African-American: 4%, Asian-Pacific-Islander: 60%, Hispanic-Latino: 8%, Multiracial-Multiethnic: 9%, White: 20%, Other: 0%} 
     ]
+    district2: 
 }
